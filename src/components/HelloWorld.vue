45298e7b7a26ddea20ffc64106aa22501eb2c25c
sidney zhang <sidneyzsc@hotmail.com>
2018-10-20 17:47:12 +0800

    change content

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,92 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <h4 class="bg-primary text-white text-center p-2">
+      TO DO List
+    </h4>
+    <div class="container-fluid p-4">
+      <div class="row" v-if="filterTasks.length === 0">
+        <div class="col text-center">
+          <b>Nothing to do. good!</b>
+        </div>
+      </div>
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+        <div class="row" v-for="t in filterTasks" v-bind:key="t.action">
+          <div class="col">{{t.action}}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="t.done" class="form-check-input" title="是否完成">
+            {{t.done}}
+          </div>
+        </div>
+      </template>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control" title="add new todo"/>
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input"/>
+          <label class="form-check-label font-weight-bold">
+            Hide completed tasks
+          </label>
+        </div>
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning" v-on:click="deleteComplete">
+            Delete Completed
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<script>
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                name: "Adam",
+                tasks: [],
+                hideCompleted: true,
+                newItemText: ""
+            }
+        },
+        computed: {
+            filterTasks() {
+                return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
+            }
+        },
+        methods: {
+            storeData() {
+                localStorage.setItem("todos", JSON.stringify(this.tasks));
+            },
+            addNewTodo() {
+                this.tasks.push({
+                    action: this.newItemText,
+                    done: false
+                });
+                this.storeData();
+                this.newItemText = ""
+            },
+            deleteComplete() {
+                this.tasks = this.tasks.filter(t => !t.done);
+                this.storeData();
+            }
+        },
+        created() {
+            let data = localStorage.getItem("todos");
+            if (data != null) {
+                this.tasks = JSON.parse(data);
+            }
+        }
+    }
+</script>
